c7bf80b618baf1c20006a6951b735f458c177736
shamshath begum <sham29.b@gmail.com>
2023-08-30 17:16:42 +0530

    hi

--- src/page/Cart.js
+++ src/page/Cart.js
@@ -6,6 +6,7 @@ import emptyCartImage from "../assest/empty.gif"
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import {loadStripe} from '@stripe/stripe-js'
+import { url } from '../App'
 
 function Cart() {
   let products=useSelector((state)=>state.product.cartItem)
@@ -29,7 +30,7 @@ function Cart() {
   let handlePayment=async()=>{
     if(user.email){
       let stripePromise=await loadStripe("pk_test_51N3eh3SHNBZiBdpKyytzJvTcbdB1Sl6pV1mkY61hbN78GC8ZiRnvrCUG8PyaclfbB2V34qwMzi7XgFZVtddk7Y4W000yKPhVH8")
-      let res=await fetch("http://localhost:8080/checkout-payment",{
+      let res=await fetch(`${url}/checkout-payment`,{
         method:"post",
         headers:{
           "content-type":"application/json"
